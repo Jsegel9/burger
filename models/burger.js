cb669511a88e5a9485a5abe5c7722078d0d4d2dc
Jsegel <segeljosh@gmail.com>
2020-01-29 22:15:39 -0600

    completed burger model

--- models/burger.js
+++ models/burger.js
@@ -2,13 +2,19 @@ var orm = require("../config/orm");
 
 var burger = {
     all: function(cb){
-
+        orm.selectAll(function(res){
+            cb(res);
+        });
     },
-    insert: function(){
-
+    insert: function(name, cb){
+        orm.insertOne(name, function(res){
+            cb(res);
+        });
     },
-    update: function(){
-
+    update: function(col, val, id, cb){
+        orm.updateOne(col, val, id, function(res){
+            cb(res);
+        });
     }
 }
 
