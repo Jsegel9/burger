825fcd64affe92395e48a4695368a26b818e60da
Jsegel <segeljosh@gmail.com>
2020-01-29 21:44:34 -0600

    adding burger model

--- models/burger.js
+++ models/burger.js
@@ -1,1 +1,15 @@
 var orm = require("../config/orm");
+
+var burger = {
+    all: function(cb){
+
+    },
+    insert: function(){
+
+    },
+    update: function(){
+
+    }
+}
+
+module.exports = burger;
