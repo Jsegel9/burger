c898e1d255969d2240f81de0a5df43761ca33203
Jsegel <segeljosh@gmail.com>
2020-01-29 22:47:41 -0600

    server set up

--- server.js
+++ server.js
@@ -1,3 +1,20 @@
 const express = require("express");
-// const exphbs = require("express-handlebars");
+const exphbs = require("express-handlebars");
 // const mysql = required("mysql2")
+
+var PORT = process.env.PORT || 8080;
+var app = express();
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgers_controllers");
+app.use(routes);
+
+app.listen(PORT, function(){
+    console.log("Server listening on http://localhost:" + PORT);
+});
