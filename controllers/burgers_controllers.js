804e96c217daf153a49c56fe23c532d794bd2afb
Jsegel <segeljosh@gmail.com>
2020-01-29 23:28:33 -0600

    controller set up and routes rendering index to / route

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -3,3 +3,14 @@ var burger = require("../models/burger");
 
 var router = express.Router();
 
+router.get("/", function(req, res){
+    burger.all(function(data){
+        var burgObj = {
+            burgers: data
+        };
+        console.log(burgObj);
+        res.render("index", burgObj);
+    })
+})
+
+module.exports = router;
